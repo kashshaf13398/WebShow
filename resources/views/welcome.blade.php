672907ec01d1a84abba11deb13e73e2a803a3669
kashshaf13398 <sakif1327@gmail.com>
2021-07-04 08:22:07 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Basic -->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <!-- Site Metas -->
+    <title>Web</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="#" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="#" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="frontend/css/bootstrap.min.css" />
+    <!-- Pogo Slider CSS -->
+    <link rel="stylesheet" href="frontend/css/pogo-slider.min.css" />
+    <!-- Site CSS -->
+    <link rel="stylesheet" href="frontend/css/style.css" />
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="frontend/css/responsive.css" />
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="frontend/css/custom.css" />
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="inner_page" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
+
+    <!-- LOADER -->
+    <!--<div id="preloader">
+        <div class="loader">
+            <img src="images/loader.gif" alt="#" />
+        </div>
+    </div>-->
+    <!-- end loader -->
+    <!-- END LOADER -->
+
+    <!-- Start header -->
+    <header class="top-header">
+        <nav class="navbar header-nav navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href=""><img src="frontend/images/i2.png" alt="image"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-wd" aria-controls="navbar-wd" aria-expanded="false" aria-label="Toggle navigation">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbar-wd">
+                    <ul class="navbar-nav">
+                        <li><a class="nav-link active" href="">Home</a></li>
+                        <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        
+                    </ul
+                    </ul>
+                        
+                    </ul>
+                </div>
+                <div class="search-box">
+                    <input type="text" class="search-txt" placeholder="Search">
+                    <a class="search-btn">
+                        <img src="frontend/images/search_icon.png" alt="#" />
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!-- End header -->
+
+	<!-- section -->
+	
+	
+	<!-- end section -->
+   
+	<!-- section -->
+    <div class="section layout_padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="full">
+                        <div class="heading_main text_align_center">
+						   <h2><span>All Products</span></h2>
+                        </div>
+					  </div>
+                </div>
+				<div class="col-md-4">
+                    <div class="full blog_img_popular">
+                       <a href="https://www.amazon.com/Razer-Viper-Ultralight-Gaming-Mouse/dp/B084RPZD6T/ref=sr_1_1?dchild=1&keywords=razer+viper+mini&qid=1625362189&s=books&sr=1-1">
+                           <img class="img-responsive" src="frontend/images/viper-mini-01-500x500.png" alt="#" /> 
+                       </a>
+					   <h4>Razer Viper Mini Ultra-Lightweight Chroma RGB Gaming Mouse</h4>
+                       <hr>
+                       <h4>Price: 2,500৳</h4>
+                    </div>
+                </div>
+				<div class="col-md-4">
+                    <div class="full blog_img_popular">
+                        <a href="https://www.amazon.com/Logitech-Wireless-Gaming-Esports-Performance/dp/B07GCKQD77/ref=sr_1_9?crid=3U0QGB0HSOUAS&dchild=1&keywords=logitech+mouse&qid=1625362436&s=computers-intl-ship&sprefix=logitech+mo%2Ccomputers-intl-ship%2C428&sr=1-9">
+                            <img class="img-responsive" src="frontend/images/g-pro-wireless-01-500x500.png" alt="#" />
+                        </a>
+					    <h4>Logitech G Pro Wireless Gaming Mouse</h4>
+                        <hr>
+                        <h4>Price: 12,000৳</h4>
+                    </div>
+                </div>
+				<div class="col-md-4">
+                    <div class="full blog_img_popular">
+                        <a href="https://www.amazon.com/Logitech-G502-Performance-Gaming-Mouse/dp/B07GBZ4Q68/ref=sr_1_1?dchild=1&keywords=logitech+mouse&qid=1625362970&sr=8-1">
+                            <img class="img-responsive" src="frontend/images/g502-hero-1-500x500.png" alt="#" />
+                        </a>
+						<h4>Logitech G502 HERO High Performance RGB Gaming Mouse</h4>
+                        <hr>
+                        <h4>Price: 6,500৳</h4>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="full blog_img_popular">
+                        <a href="https://www.amazon.com/MSI-FreeSync-Non-Glare-Optix-G241VC/dp/B07QR7KTG2/ref=sr_1_1?dchild=1&keywords=MSI+Optix+G241VC&qid=1625363537&sr=8-1">
+                            <img class="img-responsive" src="frontend/images/g241vc-1-500x500.png" alt="#" />
+                        </a>
+						<h4>MSI Optix G241VC 24-Inch Full HD Curved Gaming Monitor</h4>
+                        <hr>
+                        <h4>Price: 18,000৳</h4>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="full blog_img_popular">
+                        <a href="https://www.amazon.com/VP28UQG-Monitor-3840x2160-Adaptive-FreeSync/dp/B074PK4R2H/ref=sr_1_6?dchild=1&keywords=Asus+VZ279HEG1R&qid=1625363679&sr=8-6">
+                            <img class="img-responsive" src="frontend/images/vz279heg1r-01-500x500.png" alt="#" />
+                        </a>
+						<h4>Asus VZ279HEG1R 27" Full HD IPS Gaming Monitor</h4>
+                        <hr>
+                        <h4>Price: 26,000৳</h4>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="full blog_img_popular">
+                        <a href="https://www.amazon.com/ASUS-XG279Q-Compatible-DisplayHDR-DisplayPort/dp/B083Z8JXW6/ref=sr_1_2?dchild=1&keywords=Asus+ROG+Strix+XG248Q&qid=1625363943&sr=8-2">
+                            <img class="img-responsive" src="frontend/images/rog-strix-xg248q-1-500x500.png" alt="#" />
+                        </a>
+						<h4>Asus ROG Strix XG248Q 24 Inch FHD Gaming Monitor</h4>
+                        <hr>
+                        <h4>Price: 39,500৳</h4>
+                    </div>
+                </div>
+				<div class="col-md-4">
+                    <div class="full blog_img_popular">
+                        <a href="https://www.amazon.com/BenQ-EW2480-24-inch-Eye-Care-Speakers/dp/B07WQBC96R/ref=sr_1_1?dchild=1&keywords=BenQ+EW2480&qid=1625364103&sr=8-1">
+                            <img class="img-responsive" src="frontend/images/ew2480-1-500x500.png" alt="#" />
+                        </a>
+						<h4>BenQ EW2480 23.8" Eye-Care IPS Monitor</h4>
+                        <hr>
+                        <h4>Price: 20,500৳</h4>
+                    </div>
+                </div>
+				<div class="col-md-4">
+                    <div class="full blog_img_popular">
+                        <a href="https://www.amazon.com/Razer-Ultimate-Wireless-Controller-Interchangeable/dp/B07N16TRPT/ref=sr_1_2?dchild=1&keywords=Razer+Raiju+Ultimate+PS4+Controller&qid=1625364351&sr=8-2">
+                            <img class="img-responsive" src="frontend/images/raiju-500x500.png" alt="#" />
+                        </a>
+						<h4>Razer Raiju Ultimate PS4 Controller with Bluetooth and Wired Connection</h4>
+                        <hr>
+                        <h4>Price: 20,500৳</h4>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="full blog_img_popular">
+                        <a href="https://www.amazon.com/Razer-Wolverine-Tournament-Remappable-Multi-Function-Controller/dp/B076ZC7KQM/ref=sr_1_2?dchild=1&keywords=Razer+Wolverine+Tournament+Edition&qid=1625364669&sr=8-2">
+                            <img class="img-responsive" src="frontend/images/razer-wolverine-gamepad-1-500x500.png" alt="#" />
+                        </a>
+						<h4>Razer Wolverine Tournament Edition Chroma - Customizable Gamepad Controller</h4>
+                        <hr>
+                        <h4>Price: 9,400৳</h4>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="full blog_img_popular">
+                        <a href="https://www.amazon.com/Razer-Blade-Gaming-Laptop-Thunderbolt/dp/B087LW2KFG/ref=sr_1_7?dchild=1&keywords=Razer+Blade+15&qid=1625364947&sr=8-7">
+                            <img class="img-responsive" src="frontend/images/blade-15-base-model-core-i7-rtx-2070-max-q-1-500x500.png" alt="#" />
+                        </a>
+						<h4>Razer Blade 15 Base Model Core i7 10th Gen 512GB SSD RTX 2070 8GB MAX-Q Graphics 15.6″ FHD Gaming Laptop</h4>
+                        <hr>
+                        <h4>Price: 194,000৳</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+	<!-- end section -->
+	<!-- section -->
+   
+	<!-- end section -->
+    <!-- Start Footer -->
+    
+
+    
+
+    <a href="#" id="scroll-to-top" class="hvr-radial-out"><i class="fa fa-angle-up"></i></a>
+
+    <!-- ALL JS FILES -->
+    <script src="frontend/js/jquery.min.js"></script>
+	<script src="frontend/js/popper.min.js"></script>
+    <script src="frontend/js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+    <script src="frontend/js/jquery.magnific-popup.min.js"></script>
+    <script src="frontend/js/jquery.pogo-slider.min.js"></script>
+    <script src="frontend/js/slider-index.js"></script>
+    <script src="frontend/js/smoothscroll.js"></script>
+    <script src="frontend/js/form-validator.min.js"></script>
+    <script src="frontend/js/contact-form-script.js"></script>
+    <script src="frontend/js/isotope.min.js"></script>
+    <script src="frontend/js/images-loded.min.js"></script>
+    <script src="frontend/js/custom.js"></script>
+	
+
+
+</body>
+
+</html>
